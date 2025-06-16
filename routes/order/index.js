d9aa2c8857838927f9ba7e96b969696a81f6f3ab
Kashif <kashifdeshmukh786@gmail.com>
2025-06-16 23:27:35 +0530

    refactor: comment out unused order route handlers

--- routes/order/index.js
+++ routes/order/index.js
@@ -7,15 +7,15 @@ const {
   adminOrSuperAdmin,
 } = require("../../middleware/auth/adminMiddleware.js");
 
-router.get("/export", superAdmin, OrderController.exportOrders);
+// router.get("/export", superAdmin, OrderController.exportOrders);
 router.get("/", adminOrSuperAdmin, OrderController.getAllOrders);
-router.get("/overview", adminOrSuperAdmin, OrderController.getOrderOverview);
+// router.get("/overview", adminOrSuperAdmin, OrderController.getOrderOverview);
 
 router.post("/", user, OrderController.createOrder);
-router.post("/buy-now", user, OrderController.buyNowOrder);
+// router.post("/buy-now", user, OrderController.buyNowOrder);
 router.get("/history", user, OrderController.getOrderHistory);
 router.patch("/:id", adminOrSuperAdmin, OrderController.updateOrder);
 router.get("/:id", adminOrSuperAdmin, OrderController.getOrderById);
-router.get("/generate-order-bill/:id", user, OrderController.generateOrderBill);
+// router.get("/generate-order-bill/:id", user, OrderController.generateOrderBill);
 
 module.exports = router;
