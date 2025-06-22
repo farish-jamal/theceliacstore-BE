075a1831cc716004f675fec82673667e2236e89e
Kashif <kashifdeshmukh786@gmail.com>
2025-06-22 23:16:21 +0530

    feat: configure CORS with specific options for enhanced security

--- server.js
+++ server.js
@@ -30,8 +30,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors());
-app.use(morgan("dev"));
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));app.use(morgan("dev"));
 
 // Routes
 app.use("/api/auth/admin", authAdminRoutes);
