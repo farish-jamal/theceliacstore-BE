e76cead919ef9c67e13e26896547c1605deb2eda
Kashif <kashifdeshmukh786@gmail.com>
2025-10-03 00:51:14 +0530

    refactor: increase JSON and URL-encoded request size limits for improved data handling

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ connectDB();
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
   origin: "*",
