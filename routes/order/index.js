ba8564a2e3203472c3d53a4534b9cd50a1e92a41
farish-jamal <farishjamal98@gmail.com>
2025-10-25 02:49:20 +0530

    fix: update generate-payment-link route to require admin or super admin access

--- routes/order/index.js
+++ routes/order/index.js
@@ -22,6 +22,6 @@ router.patch("/edit/:id", user, OrderController.editOrder);
 router.get("/:id", adminOrSuperAdmin, OrderController.getOrderById);
 router.get("/user/:id", user, OrderController.getOrderByIdFormUser);
 // router.get("/generate-order-bill/:id", user, OrderController.generateOrderBill);
-router.post("/generate-payment-link", user, OrderController.generatePaymentLinks);
+router.post("/generate-payment-link", adminOrSuperAdmin, OrderController.generatePaymentLinks);
 
 module.exports = router;
